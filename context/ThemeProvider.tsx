87ef9104396326fee2a89ef6cd9fcd58115724cf
Negar Parvaresh <negar.prv@gmail.com>
2023-12-10 18:03:03 +0330

    navbar and theme fixed

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -13,12 +13,18 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
+      document.documentElement.classList.remove("light");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
     }
   };
 
