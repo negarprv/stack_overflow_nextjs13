2c9a20928e1bdce74939571ce9fcdf9b58f2bf14
Negar Parvaresh <negar.prv@gmail.com>
2024-02-03 10:58:18 +0330

    changed

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,16 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhooks(.*)"],
-  ignoredRoutes: ["/api/webhooks", "/api/chatgpt"],
+  ignoredRoutes: [
+    "/api/webhooks",
+    "/api/chatgpt",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
 });
 
 export const config = {
