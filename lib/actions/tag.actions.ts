eee3733a26d788545c27ad9eb8d1f61639056660
Negar Parvaresh <negar.prv@gmail.com>
2024-02-06 10:08:59 +0330

    community and tags pages finished

---
+++ lib/actions/tag.actions.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import User from "@/database/user.model";
+import { connectToDB } from "../mongoose";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
+
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    connectToDB();
+
+    const { userId, limit = 3 } = params;
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found");
+
+    // find interactions for the user and group by tags...
+
+    return [
+      { _id: "1", name: "tag1" },
+      { _id: "2", name: "tag2" },
+      { _id: "3", name: "tag3" },
+    ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDB();
+
+    const tags = await Tag.find({});
+
+    return { tags };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
